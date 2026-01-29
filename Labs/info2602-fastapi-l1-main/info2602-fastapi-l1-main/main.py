21e507b1516691992b803858f632be42accf2d82
Stars0-0 <85362134+Stars0-0@users.noreply.github.com>
2026-01-28 21:02:27 -0400

    Add student filtering and basic math operations

--- Labs/info2602-fastapi-l1-main/info2602-fastapi-l1-main/main.py
+++ Labs/info2602-fastapi-l1-main/info2602-fastapi-l1-main/main.py
@@ -13,8 +13,59 @@ with open('./data.json') as f:
 def hello_world():
     return 'Hello, World!'
 
-@app.get('/students/{id}')
-async def get_student(id):
-  for student in data: 
-    if student['id'] == id: # Only return the student if the ID matches
-      return student
+@app.get('/students')
+async def get_students(pref=None):
+    if pref:
+        filtered_students = []
+        for student in data:
+            if student['pref'] == pref: # select only the students with a given meal preference
+            filtered_students.append(student) # add match student to the result
+        return filtered_students
+    return data
+
+#Exercise 1
+
+@app.get('/stats')
+async def get_stats():
+    meal_stats = {}
+    programme_stats = {}
+
+    for student in data:
+        meal = student['pref']
+        programme = student['programme']
+
+        if meal in meal_stats:
+            meal_stats[meal] += 1
+        else:
+            meal_stats[meal] = 1
+
+        if programme in programme_stats:
+            programme_stats[programme] += 1
+        else:
+            programme_stats[programme] = 1
+
+    return {
+        "meal_preferences": meal_stats,
+        "programmes": programme_stats
+    }
+
+
+#Exercise 2
+
+@app.get('/add/{a}/{b}')
+def add(a: int, b: int):
+    return a + b
+
+@app.get('/subtract/{a}/{b}')
+def subtract(a: int, b: int):
+    return a - b
+
+@app.get('/multiply/{a}/{b}')
+def multiply(a: int, b: int):
+    return a * b
+
+@app.get('/divide/{a}/{b}')
+def divide(a: int, b: int):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    return a / b
